ccb7e25748f93680b5b481fe69836bfa3e63fa89
David Ruiz Lopez <davidruizlopez2501@gmail.com>
2020-02-17 15:30:31 +0100

    add the link

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'new', to: 'game#new'
+  get 'new', to: 'game#new', as: :new
   post 'score', to: 'game#score'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
